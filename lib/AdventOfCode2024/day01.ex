94ab712c9b3c8f78e9181ff5834abc528d22abbf
Adam Michalski <aadam.michalski@gmail.com>
2024-12-01 09:25:55 +0000

    The potion of the first day
    :)_

--- lib/AdventOfCode2024/day01.ex
+++ lib/AdventOfCode2024/day01.ex
@@ -1,19 +1,38 @@
 defmodule AdventOfCode2024.Day01 do
   @behaviour AdventOfCode.Day
 
+  def preprocess(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn pair ->
+      String.split(pair, "   ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+  end
+
   @impl AdventOfCode.Day
   def part1(input) do
     input
-    |> String.split("\n", trim: true)
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.reduce(&Kernel.+/2)
+    |> preprocess()
+    |> Enum.map(&Enum.sort/1)
+    |> Enum.zip()
+    |> Enum.map(fn {a, b} -> abs(a - b) end)
+    |> Enum.sum()
   end
 
   @impl AdventOfCode.Day
   def part2(input) do
     input
-    |> String.split("\n", trim: true)
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.reduce(&Kernel.*/2)
+    |> preprocess()
+    |> then(fn [left, right] ->
+      [
+        left,
+        Enum.reduce(right, %{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
+      ]
+    end)
+    |> then(fn [left, map] -> Enum.map(left, fn x -> x * Map.get(map, x, 0) end) end)
+    |> Enum.sum()
   end
 end
