bfb46bf66db43526a86434bc68a8f49189228ccf
Adam Michalski <aadam.michalski@gmail.com>
2024-12-06 08:41:46 +0000

    Optimize d6p2 a bit
    it now takes 15s instead of 2m

--- lib/AdventOfCode2024/day06.ex
+++ lib/AdventOfCode2024/day06.ex
@@ -2,7 +2,7 @@ defmodule AdventOfCode2024.Day06 do
   @behaviour AdventOfCode.Day
 
 
-  def walk_grid(grid) do
+  def walk_grid(grid, extra_obstacle \\ nil) do
     start_pos =
       Grid.all_cells(grid)
       |> Enum.find(fn {row, col} -> Grid.get(grid, {row, col}) == "^" end)
@@ -19,16 +19,16 @@ defmodule AdventOfCode2024.Day06 do
     Stream.iterate({start_pos, start_dir, MapSet.new(), MapSet.new()}, fn {pos = {row, col}, dir = {drow, dcol},
                                                              visited, visited_with_dir} ->
       if MapSet.member?(visited_with_dir, {pos, dir}) do
-        {:halt, :loop}
+        {:halt, :loop, visited}
       else
         visited = MapSet.put(visited, pos)
         visited_with_dir = MapSet.put(visited_with_dir, {pos, dir})
         next_pos = {row + drow, col + dcol}
 
         if not Grid.is_valid_position(grid, next_pos) do
-          {:halt, MapSet.size(visited)}
+          {:halt, MapSet.size(visited), visited}
         else
-          if Grid.get(grid, next_pos) == "#" do
+          if Grid.get(grid, next_pos) == "#" or next_pos == extra_obstacle do
             {pos, elem(Map.fetch(next_dir, dir), 1), visited, visited_with_dir}
           else
             {next_pos, dir, visited, visited_with_dir}
@@ -37,10 +37,9 @@ defmodule AdventOfCode2024.Day06 do
       end
       end)
       |> Enum.find(fn
-        {:halt, _} -> true
+        {:halt, _, _} -> true
         _ -> false
       end)
-      |> elem(1)
 
   end
   @impl AdventOfCode.Day
@@ -50,20 +49,23 @@ defmodule AdventOfCode2024.Day06 do
       |> Grid.strings_to_grid()
 
     walk_grid(grid)
+      |> elem(1)
   end
 
   @impl AdventOfCode.Day
   def part2(input) do
     grid = String.split(input, "\n", trim: true) |> Grid.strings_to_grid()
     # This is disgusting lol yolo
-    Grid.all_cells(grid)
+    walk_grid(grid)
+    |> elem(2)
     |> Enum.map(fn pos = {row, col} ->
       c = Grid.get(grid, {row, col})
       if c == "#" or c == "^" do
         false
       else
-        grid = Grid.set(grid, pos, "#")
-        walk_grid(grid) == :loop
+        walk_grid(grid, pos)
+        |> elem(1)
+        |> then(fn x -> x == :loop end)
       end
     end)
     |> Enum.count(& &1)
