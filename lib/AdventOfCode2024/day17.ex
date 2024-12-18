328db2aeee1b00977c831543a0c0f807a7cf539b
Adam Michalski <aadam.michalski@gmail.com>
2024-12-18 11:53:01 +0000

    18
    :)

--- lib/AdventOfCode2024/day17.ex
+++ lib/AdventOfCode2024/day17.ex
@@ -156,6 +156,7 @@ defmodule AdventOfCode2024.Day17 do
         end)
       end)
     end)
+    |> IO.inspect()
     |> Enum.map(fn i -> String.to_integer(i, 2) end)
     |> Enum.min()
   end
--- lib/AdventOfCode2024/day18.ex
+++ lib/AdventOfCode2024/day18.ex
@@ -1,13 +1,93 @@
 defmodule AdventOfCode2024.Day18 do
   @behaviour AdventOfCode.Day
 
+  def in_bounds?({x, y}) do
+    x >= 0 and x <= 70 and y >= 0 and y <= 70
+  end
+
+  def path_length(obstacles) do
+    q = :queue.from_list([{{0, 0}, 0}])
+
+    Stream.iterate({:cont, {q, MapSet.new()}}, fn {_, {q, visited}} ->
+      v = :queue.out(q)
+
+      case v do
+        {:empty, _} ->
+          {:error, :error}
+
+        {{:value, {pos = {row, col}, t}}, q} ->
+          if MapSet.member?(visited, pos) do
+            {:cont, {q, visited}}
+          else
+            visited = MapSet.put(visited, pos)
+
+            if pos == {70, 70} do
+              {:done, t}
+            else
+              q =
+                Enum.reduce(
+                  [{0, 1}, {1, 0}, {0, -1}, {-1, 0}],
+                  q,
+                  fn {drow, dcol}, q ->
+                    npos = {row + drow, col + dcol}
+
+                    if in_bounds?(npos) and not MapSet.member?(visited, npos) and
+                         not MapSet.member?(obstacles, npos) do
+                      :queue.in({npos, t + 1}, q)
+                    else
+                      q
+                    end
+                  end
+                )
+
+              {:cont, {q, visited}}
+            end
+          end
+      end
+    end)
+    |> Enum.find(fn {signal, _} -> signal == :done or signal == :error end)
+    |> then(fn {signal, t} ->
+      if signal == :done do
+        t
+      else
+        :error
+      end
+    end)
+  end
+
   @impl AdventOfCode.Day
   def part1(input) do
-    "not implemented"
+    obstacles =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        String.split(line, ",", trim: true) |> Enum.map(&String.to_integer/1)
+      end)
+      |> Enum.map(&List.to_tuple/1)
+
+    obstacles = obstacles |> Enum.take(1024) |> Enum.into(MapSet.new())
+
+    path_length(obstacles)
   end
 
   @impl AdventOfCode.Day
   def part2(input) do
-    "not implemented"
+    obstacles =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        String.split(line, ",", trim: true) |> Enum.map(&String.to_integer/1)
+      end)
+      |> Enum.map(&List.to_tuple/1)
+
+    Stream.iterate({1025, :ok}, fn {i, _} ->
+      obstacles = obstacles |> Enum.take(i) |> Enum.into(MapSet.new())
+
+      {i + 1, path_length(obstacles)}
+    end)
+    |> Enum.find(fn {_, signal} -> signal == :error  end)
+    |> elem(0)
+    |> then(&Enum.at(obstacles, &1 - 2))
+    |> then(fn i -> "#{elem(i, 0)},#{elem(i, 1)}" end)
   end
 end
